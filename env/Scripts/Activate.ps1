b2f23c2428ee5179fbcaddc79a33941f38140fef
ilya zahvatkin <il.zahvatkin@outlook.com>
2020-08-04 20:45:14 +0700

    env

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="E:\programms\flask\env"
+$env:VIRTUAL_ENV="C:\Users\Татьяна\Desktop\Zik\python\reminder\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
