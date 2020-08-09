e2882690ea0ef058e963f6d47bd4371d1f9bc266
Zikil <il.zahvatkin33@gmail.com>
2020-08-09 11:57:59 +0700

    six final

---
+++ migrations/versions/5c3ba0549b40_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5c3ba0549b40
+Revises: 
+Create Date: 2020-08-08 16:48:40.513712
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5c3ba0549b40'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
